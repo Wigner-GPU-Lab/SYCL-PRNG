9a42bbf67d64ccd760ec0eca2698c20d10341e77
Nagy-Egri Máté Ferenc <csiga.biga@aol.com>
2018-08-02 15:14:27 +0200

    TinyMT 32 prototype complete

--- include/PRNG/TinyMT.hpp
+++ include/PRNG/TinyMT.hpp
@@ -26,6 +26,7 @@ namespace prng
         {
             state_[0] = value ^ ((result_type)mat1 << 32);
             state_[1] = mat2 ^ tmat;
+
             for (int i = 1; i < min_loop; i++)
             {
                 state_[i & 1] ^=
@@ -107,5 +108,116 @@ namespace prng
         }
     };
 
-    using tinymt_64 = tiny_mersenne_twister_engine_64<12, 11, 8>;
+    template <std::uint32_t Mat1, std::uint32_t Mat2, std::uint32_t TMat>
+    class tiny_mersenne_twister_engine_32
+    {
+    public:
+
+        using result_type = std::uint32_t;
+
+        static constexpr std::size_t word_size = 32;
+        static constexpr std::size_t state_size = 4;
+        static constexpr result_type mask = 0x7fffffff;
+        static constexpr std::size_t sh0 = 1;
+        static constexpr std::size_t sh1 = 10;
+        static constexpr std::size_t sh8 = 8;
+
+        static constexpr result_type default_seed = 5489u;
+
+        tiny_mersenne_twister_engine_32(result_type value)
+        {
+            state_[0] = value;
+            state_[1] = mat1;
+            state_[2] = mat2;
+            state_[3] = tmat;
+
+            for (int i = 1; i < min_loop; i++)
+            {
+                state_[i & 3] ^= i + UINT32_C(1812433253) *
+                    (state_[(i - 1) & 3] ^ (state_[(i - 1) & 3] >> 30));
+            }
+
+            for (int i = 0; i < pre_loop; i++) next_state();
+        }
+
+        template <typename Sseq> explicit tiny_mersenne_twister_engine_32(Sseq& s);
+
+        tiny_mersenne_twister_engine_32() : tiny_mersenne_twister_engine_32(default_seed) {}
+        tiny_mersenne_twister_engine_32(const tiny_mersenne_twister_engine_32&) = default;
+
+        void seed(result_type value = default_seed);
+        template <typename Sseq> void seed(Sseq& s);
+
+        result_type operator()()
+        {
+            next_state();
+
+            return temper();
+        }
+
+        void discard(unsigned long long z) { for (; 0 < z; --z) this->operator(); }
+
+        friend bool operator==(const tiny_mersenne_twister_engine_32<Mat1, Mat2, TMat>& lhs,
+                               const tiny_mersenne_twister_engine_32<Mat1, Mat2, TMat>& rhs)
+        {
+            return (lhs.state_[0] == rhs.state_[0]) &&
+                   (lhs.state_[1] == rhs.state_[1]) &&
+                   (lhs.state_[2] == rhs.state_[2]) &&
+                   (lhs.state_[3] == rhs.state_[3]);
+        }
+
+        friend bool operator!=(const tiny_mersenne_twister_engine_32<Mat1, Mat2, TMat>& lhs,
+                               const tiny_mersenne_twister_engine_32<Mat1, Mat2, TMat>& rhs)
+        {
+            return (lhs.state_[0] != rhs.state_[0]) ||
+                   (lhs.state_[1] != rhs.state_[1]) ||
+                   (lhs.state_[2] != rhs.state_[2]) ||
+                   (lhs.state_[3] != rhs.state_[3]);
+        }
+
+        static constexpr result_type min() { return std::numeric_limits<result_type>::min(); }
+        static constexpr result_type max() { return std::numeric_limits<result_type>::max(); }
+
+    private:
+
+        result_type state_[state_size];
+
+        // Convenience renames
+        static constexpr auto mat1 = Mat1;
+        static constexpr auto mat2 = Mat2;
+        static constexpr auto tmat = TMat;
+
+        // Non-customizable params
+        static constexpr int min_loop = 8;
+        static constexpr int pre_loop = 8;
+
+        inline void next_state()
+        {
+            result_type x = (state_[0] & mask) ^ state_[1] ^ state_[2],
+                        y = state_[3];
+
+            x ^= (x << sh0);
+            y ^= (y >> sh0) ^ x;
+
+            state_[0] = state_[1];
+            state_[1] = state_[2];
+            state_[2] = x ^ (y << sh1);
+            state_[3] = y;
+            state_[1] ^= -((std::int32_t)(y & 1)) & mat1;
+            state_[2] ^= -((std::int32_t)(y & 1)) & mat2;
+        }
+        inline result_type temper()
+        {
+            result_type t0 = state_[3],
+                        t1 = state_[0] + (state_[2] >> sh8);
+
+            t0 ^= t1;
+            t0 ^= -((std::int32_t)(t1 & 1)) & tmat;
+
+            return t0;
+        }
+    };
+
+    using tinymt_64 = tiny_mersenne_twister_engine_64<0xd02f1a04, 0xfe80ffa0, 0x71126defef7e7ffa>; // tinymt64dc --count 1 1
+    using tinymt_32 = tiny_mersenne_twister_engine_32<0xda251b45, 0xfed0ffb5, 0x9b5cf7ff>;         // tinymt32dc --count 1 1
 }
