9c3d87691be53b6be109d0bc3bdb103739e832ed
Nagy-Egri Máté Ferenc <nagy-egri.mate@wigner.mta.hu>
2018-08-28 16:25:48 +0200

    Fixed SYCL example to actually build and work.

--- examples/SYCL-RandomSeed.cpp
+++ examples/SYCL-RandomSeed.cpp
@@ -12,19 +12,27 @@
 #include <algorithm>
 #include <iostream>
 
+namespace kernels
+{
+    struct skip;
+    struct step;
+
+    template <typename Engine, typename Method>
+    struct invocation;
+}
 
 int main()
 {
     static_assert(std::is_standard_layout<prng::tinymt_64>::value, "TinyMT 64 is not standard layout.");
     static_assert(std::is_standard_layout<prng::tinymt_32>::value, "TinyMT 32 is not standard layout.");
-    static_assert(std::is_standard_layout<prng::mwc64x>::value, "MWC64X is not standard layout.");
+    static_assert(std::is_standard_layout<prng::mwc64x_32>::value, "MWC64X 32 is not standard layout.");
 
     // Sample params
     const std::size_t plat_index = std::numeric_limits<std::size_t>::max();
     const std::size_t dev_index = std::numeric_limits<std::size_t>::max();
     const auto dev_type = cl::sycl::info::device_type::gpu;
     const std::size_t length = 65536u;
-    const std::size_t num = 1024;
+    const std::size_t num = 64;
 
     try
     {
@@ -57,57 +65,98 @@ int main()
         cl::sycl::queue queue{ dev };
 
         cl::sycl::buffer<std::random_device::result_type> seeds_buf{ cl::sycl::range<1>{ length } };
-        cl::sycl::buffer<prng::tinymt_64> tmt64_buf{ cl::sycl::range<1>{ length } };
-        cl::sycl::buffer<prng::tinymt_32> tmt32_buf{ cl::sycl::range<1>{ length } };
-        cl::sycl::buffer<prng::mwc64x> mwc32_buf{ cl::sycl::range<1>{ length } };
+        cl::sycl::buffer<prng::tinymt_64> tmt64{ cl::sycl::range<1>{ length } };
+        cl::sycl::buffer<prng::tinymt_32> tmt32{ cl::sycl::range<1>{ length } };
+        cl::sycl::buffer<prng::mwc64x_32> mwc32{ cl::sycl::range<1>{ length } };
 
-        std::vector<std::random_device::result_type> seeds_vec;
-        std::vector<prng::tinymt_64> tmt64_vec( length );
-        std::vector<prng::tinymt_32> tmt32_vec( length );
-        std::vector<prng::mwc64x> mwc32_vec( length );
+        cl::sycl::buffer<prng::tinymt_64> tmt64_ref{ cl::sycl::range<1>{ length } };
+        cl::sycl::buffer<prng::tinymt_32> tmt32_ref{ cl::sycl::range<1>{ length } };
+        cl::sycl::buffer<prng::mwc64x_32> mwc32_ref{ cl::sycl::range<1>{ length } };
 
         // Initialize seeds
         {
             std::random_device rd;
 
-            std::generate_n(std::back_inserter(seeds_vec), length, [&]() { return rd(); });
+            auto seeds_acc = seeds_buf.get_access<cl::sycl::access::mode::write>();
+
+            std::generate(seeds_acc.get_pointer(),
+                          seeds_acc.get_pointer() + seeds_acc.get_count(),
+                          [&]() { return rd(); });
         }
 
-        auto jump_engines = [&](auto engines_buf)
+        auto jump_engines = [&, num](auto& skip_seq, auto& step_seq)
         {
+            auto skip_function_discard = [n = num](auto& engine) { engine.discard(n); }; // discard
+            auto next_stepping_discard = [n = num](auto& engine) { for (std::int32_t i = 0; i < n; ++i) engine(); }; // operator()
+
             queue.submit([&](cl::sycl::handler& cgh)
             {
                 auto seeds = seeds_buf.get_access<cl::sycl::access::mode::read>();
-                auto engines = engines_buf.template get_access<cl::sycl::access::mode::write>();
+                auto engines = skip_seq.template get_access<cl::sycl::access::mode::write>();
 
                 using engine_type = typename decltype(engines)::value_type;
 
-                cgh.parallel_for<class SYCL_RandomSeed>(cl::sycl::range<1>{length}, [=](cl::sycl::item<1> i)
+                cgh.parallel_for<kernels::invocation<engine_type, kernels::skip>>(cl::sycl::range<1>{length}, [=](cl::sycl::item<1> i)
                 {
                     auto seed = seeds[i];
 
-                    engine_type engine{ seed };
+                    // Cast required because of ComputeCpp bug
+                    engine_type engine{ static_cast<typename engine_type::result_type>(seed) }; // random seeding
 
-                    engine.discard(num);
+                    skip_function_discard(engine); // discard
 
-                    engines[i] = engine;
+                    engines[i] = engine; // copy CTOR
+                });
+            });
+
+            // TOOD: remove code duplication
+            queue.submit([&](cl::sycl::handler& cgh)
+            {
+                auto seeds = seeds_buf.get_access<cl::sycl::access::mode::read>();
+                auto engines = skip_seq.template get_access<cl::sycl::access::mode::write>();
+
+                using engine_type = typename decltype(engines)::value_type;
+
+                cgh.parallel_for<kernels::invocation<engine_type, kernels::step>>(cl::sycl::range<1>{length}, [=](cl::sycl::item<1> i)
+                {
+                    auto seed = seeds[i];
+
+                    // Cast required because of ComputeCpp bug
+                    engine_type engine{ static_cast<typename engine_type::result_type>(seed) }; // random seeding
+
+                    next_stepping_discard(engine); // discard
+
+                    engines[i] = engine; // copy CTOR
                 });
             });
         };
 
-        jump_engines(tmt64_buf);
-        jump_engines(tmt32_buf);
-        jump_engines(mwc32_buf);
+        auto match_skip_vs_step = [=](auto& skip_seq, auto& step_seq)
+        {
+            jump_engines(skip_seq, step_seq);
 
-        // Verify
-        //{
-        //    auto access = buf.get_access<cl::sycl::access::mode::read>();
-        //
-        //    if (std::any_of(access.get_pointer(),
-        //        access.get_pointer() + access.get_count(),
-        //        [res = 1.f + 1.f + 2.f](const float& val) { return val != res; }))
-        //        throw std::runtime_error{ "Wrong result computed in kernel." };
-        //}
+            auto skip_acc = skip_seq.template get_access<cl::sycl::access::mode::read>();
+            auto step_acc = step_seq.template get_access<cl::sycl::access::mode::read>();
+
+            auto iters = std::mismatch(skip_acc.get_pointer(), skip_acc.get_pointer() + skip_acc.get_count(), step_acc.get_pointer()); // operator==
+
+            if (iters.first != (skip_acc.get_pointer() + skip_acc.get_count()) ||
+                iters.second != (step_acc.get_pointer() + step_acc.get_count()))
+            {
+                std::cerr << "Skip vs. step differs for " <<
+                    typeid(skip_acc[0]).name() <<
+                    " when discarding " <<
+                    num <<
+                    " elements." <<
+                    std::endl;
+
+                std::exit(EXIT_FAILURE);
+            }
+        };
+
+        match_skip_vs_step(tmt64, tmt64_ref);
+        match_skip_vs_step(tmt32, tmt32_ref);
+        match_skip_vs_step(mwc32, mwc32_ref);
 
         std::cout << "Result verification passed!" << std::endl;
     }
@@ -121,41 +170,6 @@ int main()
         std::cerr << e.what() << std::endl;
         std::exit(EXIT_FAILURE);
     }
-
-    std::vector<prng::tinymt_64> tmt64s{ prng::tinymt_64{} }; // default CTOR
-    std::vector<prng::tinymt_32> tmt32s{ prng::tinymt_32{} }; // default CTOR
-    std::vector<prng::mwc64x> mwc32s{ prng::mwc64x{} }; // default CTOR
-
-    {
-        std::random_device rd;
-        std::generate_n(std::back_inserter(tmt64s),
-                        10,
-                        [&]() { return prng::tinymt_64{ rd() }; }); // random seeding
-
-        std::generate_n(std::back_inserter(tmt32s),
-                        10,
-                        [&]() { return prng::tinymt_32{ rd() }; }); // random seeding
-
-        std::generate_n(std::back_inserter(mwc32s),
-                        10,
-                        [&]() { return prng::mwc64x{ rd() }; }); // random seeding
-    }
-
-    auto tmt64_it = std::find(tmt64s.cbegin(), tmt64s.cend(), prng::tinymt_64{}); // operator==
-    auto tmt32_it = std::find(tmt32s.cbegin(), tmt32s.cend(), prng::tinymt_32{}); // operator==
-    auto mwc32_it = std::find(mwc32s.cbegin(), mwc32s.cend(), prng::mwc64x{}); // operator==
-
-    prng::tinymt_64 a64;
-    prng::tinymt_32 a32;
-    std::uniform_real_distribution<> d;
-
-    std::generate_n(std::ostream_iterator<double>(std::cout, "\n"),
-                    10,
-                    [&]() { return d(a64); }); // operator()()
-
-    std::generate_n(std::ostream_iterator<double>(std::cout, "\n"),
-                    10,
-                    [&]() { return d(a32); }); // operator()()
 
     return 0;
 }
